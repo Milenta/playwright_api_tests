164cd32475570bd7b66f28a35d12d5d5997193aa
Milenta <marko1milentijevic@gmail.com>
2024-12-20 21:02:57 +0100

    add dynamic post api tests

---
+++ README.md
@@ -0,0 +1,10 @@
+Package installation command:
+
+Prettier - code formater:
+npm i prettier
+
+Faker - random data and number generator
+npm install @faker-js/faker --save-dev
+
+Luxon - random date generator
+npm install --save luxon
--- tests/restfulBooker.spec.js
+++ tests/restfulBooker.spec.js
@@ -4,8 +4,11 @@ import { faker } from '@faker-js/faker'
 const { DateTime } = require('luxon')
 const url = 'https://restful-booker.herokuapp.com'
 
-test.only('Create POST request using dinamic request body', async ({ request }) => {
-  const firstName = faker.person.firstName() // random first name
+let bookId = ''
+const firstName = faker.person.firstName() // random first name
+
+test('Create POST request using dinamic request body', async ({ request }) => {
+  
   const lastName = faker.person.lastName() // random last name
   const totalPrice = faker.number.int(1000) // random number between 0 and 1000
 
@@ -30,10 +33,29 @@ test.only('Create POST request using dinamic request body', async ({ request }) 
 
   const postApiResponseBody = await postApiResponse.json()
   console.log(postApiResponseBody)
+  console.log(postApiResponseBody.bookingid)
+  bookId = postApiResponseBody.bookingid
   // validate json api response
   expect(postApiResponseBody.booking).toHaveProperty('firstname', firstName)
   expect(postApiResponseBody.booking).toHaveProperty('lastname', lastName)
   // validate nasted json objects
   expect(postApiResponseBody.booking.bookingdates).toHaveProperty('checkin', checkInDate)
   expect(postApiResponseBody.booking.bookingdates).toHaveProperty('checkout', chedkOutDate)
+
+  const getResponse = await request.get(`${url}/booking/${bookId}`) // double check if data is created
+  expect(getResponse.status()).toBe(200)
+
+})
+
+test('Execute GET api request to get book', async({request})=>{
+  const getResponse = await request.get(`${url}/booking/${bookId}`) // double check if data is created
+  expect(getResponse.status()).toBe(200)
+
+  const getResponse2 = await request.get(`${url}/booking/`, {
+    params: {
+'firstname' : firstName
+    }
+  }) // double check if data is created
+  console.log('first name is ' + firstName)
+  expect(getResponse2.status()).toBe(200)
 })
